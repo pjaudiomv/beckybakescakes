71ad55fc6e0a38a0a90e1ceac0738db7b1390113
Patrick Joyce <pjaudiomv@gmail.com>
2021-11-19 16:49:58 -0500

    e

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -29,7 +29,7 @@
     <link rel="manifest" href="manifest.json" />
     <meta name="mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
-    <title>BEcky Bakes Cakes</title>
+    <title>Becky Bakes Cakes</title>
 </svelte:head>
 <svelte:window on:click={documentClick} on:touchend={documentClick} />
 
